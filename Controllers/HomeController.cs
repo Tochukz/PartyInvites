0142b622797836bde66c068cd6661b5e677a3460
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-06-20 01:25:50 +0200

    Party invites demon app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿//using System.Diagnostics;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting =  hour > 12 ? "Good Afternoon" : "Good Morning";
+            ViewData["Message"] = "Good Morning";
+            return View("MyView");
+            
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //There is a validation error
+                return View();
+            }
+            
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where( r => r.WillAttend == true));
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models
+{
+    
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please eneter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please eneter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Pleasse eneter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
